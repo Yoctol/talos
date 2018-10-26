8dc0943e585a69dcbd088d60059049f41a49746b
noobOriented <jsaon92@gmail.com>
2018-10-26 19:45:49 +0800

    add rank3 and invalid test case.

--- talos/layers/tests/test_layers.py
+++ talos/layers/tests/test_layers.py
@@ -1,11 +1,28 @@
+import pytest
+
 import tensorflow as tf
 
 from ..layers import Dense
 
 
-def test_dense():
-    dense_layer = Dense(units=10)
-    assert dense_layer.units == 10
-    x = tf.placeholder(dtype=tf.float32, shape=[None, 20])
-    y = dense_layer(x)
-    assert y.shape.as_list() == [None, 10]
+@pytest.fixture(scope='module')
+def dense_layer():
+    return Dense(units=10)
+
+
+def test_dense(dense_layer):
+    inputs = tf.placeholder(dtype=tf.float32, shape=[None, 20])
+    outputs = dense_layer(inputs)
+    assert outputs.shape.as_list() == [None, 10]
+
+
+def test_dense_rank3(dense_layer):
+    inputs = tf.placeholder(dtype=tf.float32, shape=[None, 5, 20])
+    outputs = dense_layer(inputs)
+    assert outputs.shape.as_list() == [None, 5, 10]
+
+
+def test_dense_invalid_input_dim(dense_layer):
+    inputs = tf.placeholder(dtype=tf.float32, shape=[None, 5])
+    with pytest.raises(ValueError):
+        dense_layer(inputs)
