8132c6e4a6506c96d153a144fade74d4b3155e0e
noobOriented <jsaon92@gmail.com>
2018-10-29 14:10:11 +0800

    fix the problems of tensor name.

--- talos/wrapper/spectral_norm.py
+++ talos/wrapper/spectral_norm.py
@@ -1,4 +1,5 @@
 import types
+from typing import Set
 
 import tensorflow as tf
 
@@ -17,7 +18,7 @@ def get_sn_kernel(
         kernel_matrix = kernel
 
     u_vector = layer.add_variable(
-        name=f'{kernel.name[::-2]}/u_vector',
+        name=f'{kernel.op.name}/u_vector',
         shape=(kernel_matrix.shape[0], 1),
         initializer=tf.truncated_normal_initializer(),
         trainable=False,
@@ -27,24 +28,33 @@ def get_sn_kernel(
     spectral_norm = tf.norm(v_vector, axis=0)
     new_u = tf.nn.l2_normalize(kernel_matrix @ v_vector, axis=0)
     update_u = tf.assign(
-        u_vector, new_u, name=f'{kernel.name[::-2]}/power_iter',
+        u_vector, new_u, name=f'{kernel.op.name}/power_iter',
     )
     layer.add_update(update_u)
 
-    sn_kernel = tf.truediv(kernel, spectral_norm + eps, name=f'{kernel.name[::-2]}_sn')
+    sn_kernel = tf.truediv(kernel, spectral_norm + eps, name=f'{kernel.op.name}_sn')
     return sn_kernel
 
 
-def add_spectral_norm(layer: tf.layers.Layer):
+def add_spectral_norm(
+        layer: tf.layers.Layer,
+        kernel_attribute_name: Set[str] = None,
+    ):
     assert not layer.built
     original_build = layer.build
+
+    def is_kernel_attr(key):
+        if kernel_attribute_name is not None:
+            return key in kernel_attribute_name
+        else:
+            return key.endswith(_WEIGHTS_VARIABLE_NAME)
 
     def new_build(self, input_shapes):
         original_build(input_shapes)
         self.built = False
         kernels = {
             key: get_sn_kernel(self, val)
-            for key, val in self.__dict__.items() if key.endswith('kernel')
+            for key, val in self.__dict__.items() if is_kernel_attr(key)
         }
         self.__dict__.update(kernels)
         self.built = True
