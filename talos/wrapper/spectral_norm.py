705305571819bf4486ef706b731cc75a80c4dca2
noobOriented <jsaon92@gmail.com>
2018-10-29 15:21:51 +0800

    remove add_spectral_norm v1, add test cases.

--- talos/wrapper/spectral_norm.py
+++ talos/wrapper/spectral_norm.py
@@ -6,68 +6,12 @@ import tensorflow as tf
 _WEIGHTS_VARIABLE_NAME = "kernel"
 
 
-def _get_sn_kernel(
-        layer: tf.layers.Layer,
-        kernel: tf.Variable,
-        eps: float = 1e-8,
-    ) -> tf.Variable:
-    rank = len(kernel.shape.as_list())
-    if rank > 2:
-        kernel_matrix = tf.reshape(kernel, [-1, kernel.shape[-1]])
-    else:
-        kernel_matrix = kernel
-
-    u_vector = layer.add_variable(
-        name=f'{kernel.op.name}/singular_vector',
-        shape=(kernel_matrix.shape[0], 1),
-        initializer=tf.truncated_normal_initializer(),
-        trainable=False,
-        dtype=layer.dtype,
-    )
-    v_vector = tf.matmul(kernel_matrix, u_vector, transpose_a=True)
-    spectral_norm = tf.norm(v_vector, axis=0)
-    new_u = tf.nn.l2_normalize(kernel_matrix @ v_vector, axis=0)
-    update_u = tf.assign(
-        u_vector, new_u, name=f'{kernel.op.name}/power_iter',
-    )
-    layer.add_update(update_u)
-
-    sn_kernel = tf.truediv(kernel, spectral_norm + eps, name=f'{kernel.op.name}_sn')
-    return sn_kernel
-
-
 def add_spectral_norm(
         layer: tf.layers.Layer,
-        kernel_attribute_name: Set[str] = None,
-    ):
-    # Very Evil HACK
-    assert not layer.built, "Can't add spectral norm on built layer!"
-    original_build = layer.build
-
-    def is_kernel_attr(key):
-        if kernel_attribute_name is not None:
-            return key in kernel_attribute_name
-        else:
-            return key.endswith(_WEIGHTS_VARIABLE_NAME)
-
-    def new_build(self, input_shapes):
-        original_build(input_shapes)
-        self.built = False
-        kernels = {
-            key: _get_sn_kernel(self, kernel)
-            for key, kernel in self.__dict__.items() if is_kernel_attr(key)
-        }
-        self.__dict__.update(kernels)
-        self.built = True
-
-    layer.build = types.MethodType(new_build, layer)
-
-
-def add_spectral_norm_v2(
-        layer: tf.layers.Layer,
         kernel_name: Set[str] = None,
+        eps: float = 1e-7,
     ):
-    # Very Evil HACK
+    # Very Very Evil HACK
     assert not layer.built, "Can't add spectral norm on built layer!"
 
     def is_kernel_var(name):
@@ -79,9 +23,31 @@ def add_spectral_norm_v2(
     original_add_variable = layer.add_variable
 
     def new_add_variable(self, name, shape, **kwargs):
-        variable = original_add_variable(name, shape, **kwargs)
+        kernel = original_add_variable(name, shape, **kwargs)
         if is_kernel_var(name):
-            return _get_sn_kernel(self, variable)
-        return variable
+            assert len(shape) >= 2, "Can't apply spectral norm on variable rank < 2!"
+            if len(shape) > 2:
+                kernel_matrix = tf.reshape(kernel, [-1, kernel.shape[-1]])
+            else:
+                kernel_matrix = kernel
+
+            u_vector = original_add_variable(
+                name=f'{name}/singular_vector',
+                shape=(kernel_matrix.shape[0], 1),
+                initializer=tf.truncated_normal_initializer(),
+                trainable=False,
+                dtype=kernel.dtype,
+            )
+            v_vector = tf.matmul(kernel_matrix, u_vector, transpose_a=True)
+            spectral_norm = tf.norm(v_vector, axis=0)
+            new_u = tf.nn.l2_normalize(kernel_matrix @ v_vector, axis=0)
+            update_u = tf.assign(
+                u_vector, new_u, name=f'{name}/power_iter',
+            )
+            self.add_update(update_u)
+
+            kernel = tf.truediv(kernel, spectral_norm + eps, name=f'{name}_sn')
+
+        return kernel
 
     layer.add_variable = types.MethodType(new_add_variable, layer)
--- talos/wrapper/tests/test_spectral_norm.py
+++ talos/wrapper/tests/test_spectral_norm.py
@@ -1,22 +1,24 @@
 import tensorflow as tf
 
-from ..spectral_norm import add_spectral_norm_v2
+from ..spectral_norm import add_spectral_norm
 
 
 def test_spectral_norm_dense():
     dense_layer = tf.layers.Dense(10)
-    add_spectral_norm_v2(dense_layer)
+    add_spectral_norm(dense_layer)
 
     inputs = tf.placeholder(dtype=tf.float32, shape=[None, 20])
     dense_layer(inputs)
     assert len(dense_layer.updates) == 1
+    assert dense_layer.updates[0].name == "dense_1/kernel/power_iter:0"
+    assert dense_layer.kernel.name == "dense_1/kernel_sn:0"
     assert len(dense_layer.trainable_variables) == 2
     assert len(tf.get_collection(tf.GraphKeys.UPDATE_OPS)) == 1
 
 
 def test_spectral_norm_gru():
     gru_cell = tf.nn.rnn_cell.GRUCell(10)
-    add_spectral_norm_v2(gru_cell)
+    add_spectral_norm(gru_cell)
 
     inputs = tf.placeholder(dtype=tf.float32, shape=[None, 20])
     state = tf.placeholder(dtype=tf.float32, shape=[None, gru_cell.state_size])
