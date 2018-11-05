f367c784b62598b9e3cd4ec9d452fefde87b878a
noobOriented <jsaon92@gmail.com>
2018-11-05 13:53:58 +0800

    New Beam Interface.

--- talos/recurrent/beam_search_decode.py
+++ talos/recurrent/beam_search_decode.py
@@ -1,11 +1,8 @@
-from typing import Callable
+from typing import Callable, List
 
 import tensorflow as tf
 
 from .chain_decode import RECURRENT_CELL_TYPE, get_default_init_state
-
-
-_TIME_AXIS = 1
 
 
 def beam_search_decode(
@@ -25,68 +22,85 @@ def beam_search_decode(
 
     inputs = first_input  # shape (N, d_i)
     state = init_state  # shape (N, d_s)
-    beam_score = beam_logits = beam_class_ids = None
-    offset = tf.expand_dims(
-        tf.range(batch_size) * beam_width,
-        axis=1,
-    )  # shape (N, 1): 0, k, 2k....
+    beam = Beam(batch_size=batch_size, width=beam_width)
 
-    for t in range(maxlen):
-        logits, state = cell(inputs, state)  # shape (Nk, d_o), (Nk, d_s)
-        n_classes = logits.shape[-1].value
-        beam_score, expanded_ids = select_beam(beam_score, logits, beam_width, n_classes)
-        # both shape (N, k), expanded_ids in range [0, kV)
-        if t > 0:
+    for _ in range(maxlen):
+        output, state = cell(inputs, state)  # shape (Nk, d_o), (Nk, d_s)
+        output, class_ids = beam.update(observation=output)  # 更新 beam
+        beam.append([output, class_ids])  # 加進 beam history 中
+        state = beam.select(state)  # 幫我 tf.gather 這個東西
+        inputs = next_input_producer(output, class_ids)  # shape (N, d_i)
+
+    output_tensor, output_class_ids = beam.get_top()
+
+    return output_tensor, output_class_ids
+
+
+class Beam:
+
+    _TIME_AXIS = 1
+
+    def __init__(self, batch_size, width):
+        self.width = width
+        self.history = None
+        self.score = None
+        self.flatten_ids = None
+        self.offset = tf.expand_dims(
+            tf.range(batch_size) * width,
+            axis=1,
+        )  # shape (N, 1): 0, k, 2k....
+
+    def append(self, tensors: List):
+        tensors = [tf.expand_dims(t, axis=self._TIME_AXIS) for t in tensors]
+        if self.history is not None:
+            assert len(tensors) == len(self.history)
+            self.history = [
+                tf.concat([h, t], axis=self._TIME_AXIS)
+                for h, t in zip(self.history, tensors)
+            ]
+        else:
+            self.history = tensors
+
+    def select_beam(self, observation, n_classes):
+        new_score = tf.nn.log_softmax(observation)  # shape (Nk, V)
+        if self.score is not None:
+            # for broadcast: (Nk, 1) + (Nk, V) -> (Nk, V)
+            expanded_score = tf.reshape(self.score, shape=[-1, 1]) + new_score
+            # expansion on last dimension (Nk, V) -> (N, kV)
+            expanded_score = tf.reshape(expanded_score, shape=[-1, self.width * n_classes])
+        else:
+            expanded_score = new_score  # shape (N, V)
+        return tf.nn.top_k(expanded_score, k=self.width)
+
+    def update(self, observation: tf.Tensor):
+        n_classes = observation.shape[-1].value
+        score, expanded_ids = self.select_beam(observation, n_classes)
+        if self.score is not None:
             beam_ids = expanded_ids // n_classes  # shape (N, k), in range [0, k)
             class_ids = expanded_ids % n_classes  # shape (N, k) in range [0, V)
-            # HACK flatten the indices to fit tf.gather
-            flatten_ids = tf.reshape(
-                offset + beam_ids,  # shape (N, 1) + (N, k) -> (N, k)
+            self.flatten_ids = tf.reshape(
+                self.offset + beam_ids,  # shape (N, 1) + (N, k) -> (N, k)
                 shape=[-1],
             )  # shape (Nk, ): k elements in [0, k), k elements in [k, 2k)...
-            logits, state, beam_logits, beam_class_ids = nested_call(
-                func=tf.gather,
-                nested_var=(logits, state, beam_logits, beam_class_ids),
-                indices=flatten_ids,
-            )
         else:
-            class_ids = expanded_ids  # shape (N, k) in range [0, V)
-            logits, state = nested_call(
-                func=tile_batch,
-                nested_var=(logits, state),
-                multiplier=beam_width,
-            )
+            class_ids = expanded_ids
 
+        self.score = score
+        if self.history is not None:
+            self.history = self.select(self.history)
+
+        observation = self.select(observation)
         class_ids = tf.reshape(class_ids, shape=[-1])
-        beam_logits = concat_on_time_axis(beam_logits, logits)
-        beam_class_ids = concat_on_time_axis(beam_class_ids, class_ids)
+        return observation, class_ids
 
-        inputs = next_input_producer(logits, class_ids)  # shape (N, d_i)
+    def select(self, params):
+        if self.flatten_ids is not None:
+            return nested_call(tf.gather, params, indices=self.flatten_ids)
+        return nested_call(tile_batch, params, multiplier=self.width)
 
-    output_ids = tf.squeeze(offset)  # shape (N, )
-    output_logits = tf.gather(beam_logits, output_ids)
-    output_class_ids = tf.gather(beam_class_ids, output_ids)
-
-    return output_logits, output_class_ids
-
-
-def select_beam(beam_score, new_logits, beam_width, n_classes):
-    new_score = tf.nn.log_softmax(new_logits)  # shape (Nk, V)
-    if beam_score is None:
-        expanded_score = new_score  # shape (N, V)
-    else:
-        # for broadcast: (Nk, 1) + (Nk, V) -> (Nk, V)
-        expanded_score = tf.reshape(beam_score, shape=[-1, 1]) + new_score
-        # expansion on last dimension (Nk, V) -> (N, kV)
-        expanded_score = tf.reshape(expanded_score, shape=[-1, beam_width * n_classes])
-    return tf.nn.top_k(expanded_score, k=beam_width)
-
-
-def concat_on_time_axis(tensor, new_tensor):
-    new_tensor = tf.expand_dims(new_tensor, axis=_TIME_AXIS)
-    if tensor is None:
-        return new_tensor
-    return tf.concat([tensor, new_tensor], axis=_TIME_AXIS)  # shape (Nk, t, V)
+    def get_top(self):
+        output_ids = tf.squeeze(self.offset, axis=-1)  # shape (N, )
+        return nested_call(tf.gather, self.history, indices=output_ids)
 
 
 def nested_call(func, nested_var, *args, **kwargs):
