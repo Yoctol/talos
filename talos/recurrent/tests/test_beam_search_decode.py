d8e40231a63d0782be4371e225c109cfc8844e22
noobOriented <jsaon92@gmail.com>
2018-11-09 14:48:46 +0800

    make test maxlen lower.

--- talos/recurrent/tests/test_beam_search_decode.py
+++ talos/recurrent/tests/test_beam_search_decode.py
@@ -6,9 +6,11 @@ import tensorflow as tf
 from ..beam_search_decode import beam_search_decode
 
 
-@pytest.fixture(scope='function')
+@pytest.yield_fixture(scope="function")
 def graph():
-    return tf.Graph()
+    graph = tf.Graph()
+    with graph.as_default():
+        yield graph
 
 
 @pytest.fixture(scope='function')
@@ -22,34 +24,32 @@ def dense_layer():
 
 
 def test_beam_search_decode(graph, cell, dense_layer):
-    with graph.as_default():
-        batch_size = 7
-        first_input = tf.placeholder(shape=[batch_size, 3], dtype=tf.float32)
-        output_logits, output_word_ids = beam_search_decode(
-            cell=cell,
-            first_input=first_input,
-            maxlen=10,
-            beam_width=3,
-            next_input_producer=lambda logits, _: dense_layer(logits),
-            end_token=0,
-        )
-    assert output_logits.shape.as_list() == [batch_size, 1, 10, 5]
-    assert output_word_ids.shape.as_list() == [batch_size, 1, 10]
+    batch_size, maxlen = 7, 5
+    first_input = tf.placeholder(shape=[batch_size, 3], dtype=tf.float32)
+    output_logits, output_word_ids = beam_search_decode(
+        cell=cell,
+        first_input=first_input,
+        maxlen=maxlen,
+        beam_width=3,
+        next_input_producer=lambda logits, _: dense_layer(logits),
+        end_token=0,
+    )
+    assert output_logits.shape.as_list() == [batch_size, 1, maxlen, cell.units]
+    assert output_word_ids.shape.as_list() == [batch_size, 1, maxlen]
 
 
 def test_beam_search_decode_dynamic(graph, cell, dense_layer):
-    with graph.as_default():
-        batch_size = None
-        first_input = tf.placeholder(shape=[batch_size, 3], dtype=tf.float32)
-        dense_layer = tf.keras.layers.Dense(units=3)
-        output_tensors = beam_search_decode(
-            cell=cell,
-            first_input=first_input,
-            maxlen=10,
-            beam_width=3,
-            next_input_producer=lambda logits, _: dense_layer(logits),
-            end_token=0,
-        )
+    batch_size, maxlen = None, 5
+    first_input = tf.placeholder(shape=[batch_size, 3], dtype=tf.float32)
+    dense_layer = tf.keras.layers.Dense(units=3)
+    output_tensors = beam_search_decode(
+        cell=cell,
+        first_input=first_input,
+        maxlen=maxlen,
+        beam_width=3,
+        next_input_producer=lambda logits, _: dense_layer(logits),
+        end_token=0,
+    )
 
     with tf.Session(graph=graph) as sess:
         sess.run(tf.variables_initializer(
@@ -60,5 +60,5 @@ def test_beam_search_decode_dynamic(graph, cell, dense_layer):
             feed_dict={first_input: np.zeros([2, 3], dtype=np.float32)},
         )
 
-    assert output_logits.shape == (2, 1, 10, 5)
-    assert output_word_ids.shape == (2, 1, 10)
+    assert output_logits.shape == (2, 1, maxlen, cell.units)
+    assert output_word_ids.shape == (2, 1, maxlen)
