221fdbc5d601644a48343075c883c11a690d2d9b
noobOriented <jsaon92@gmail.com>
2018-11-12 17:16:18 +0800

    fix the bugs of renorm seqlen part.

--- talos/module/attention.py
+++ talos/module/attention.py
@@ -11,17 +11,18 @@ class GlobalAttentionPooling1D(Module):
             units,
             heads=1,
             activation='tanh',
+            use_bias=False,
             **kwargs,
         ):
         self.candidate_layer = tf.keras.layers.Dense(
             units=units,
             activation=activation,
-            use_bias=False,
+            use_bias=use_bias,
             name='candidate_layer',
         )
-        self.presoftmax_layer = tf.keras.layers.Dense(
+        self.softmax_layer = tf.keras.layers.Dense(
             units=heads,
-            activation=None,
+            activation=lambda logits: tf.nn.softmax(logits, axis=1),
             use_bias=False,
             name='presoftmax_layer',
         )
@@ -33,16 +34,18 @@ class GlobalAttentionPooling1D(Module):
             seqlen: tf.Tensor = None,
         ) -> tf.Tensor:
         if inputs.shape.ndims != 3:
-            raise ValueError()
+            raise ValueError("Input of GlobalAttentionPooling1D should be rank 3!")
         # shape (N, T, units)
         hidden_outputs = self.candidate_layer(inputs)
         # shape (N, T, Head)
-        logits = self.presoftmax_layer(hidden_outputs)
-        weights = tf.nn.softmax(logits, axis=1)
+        weights = self.softmax_layer(hidden_outputs)
         if seqlen is not None:
-            mask = tf.to_float(tf.sequence_mask(seqlen))
-            weights *= tf.expand_dims(mask, axis=-1)
-            weights /= tf.reduce_sum(weights)
+            # Renormalize for lower seqlen
+            maxlen = inputs.shape[1].value
+            # shape (N, T)
+            mask = tf.sequence_mask(seqlen, maxlen=maxlen, dtype=tf.float32)
+            weights *= tf.expand_dims(mask, axis=2)
+            weights /= tf.reduce_sum(weights, axis=1, keepdims=True)
 
         # shape (N, Head, input_dim)
         outputs = tf.matmul(weights, inputs, transpose_a=True)
