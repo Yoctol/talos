42ea4c8fa3326c8fb29af46a1868f554f34a4d1a
noobOriented <jsaon92@gmail.com>
2018-11-16 19:08:42 +0800

    specified the remove source code part.

--- talos/module/sequential.py
+++ talos/module/sequential.py
@@ -9,9 +9,11 @@ from tensorflow.python.training.checkpointable import base as checkpointable
 class Sequential(keras_Sequential):
 
     # HACK override: remove adding InputLayer part!!
-    # https://github.com/tensorflow/tensorflow/blob/r1.12/tensorflow/python/keras/engine/sequential.py#L122-L188
+    # https://github.com/tensorflow/tensorflow/blob/r1.11/tensorflow/python/keras/engine/sequential.py#L122-L188
+    # just copy/paste the source code and remove L142-L170
     @checkpointable.no_automatic_dependency_tracking
     def add(self, layer):
+        # source code L137
         if not isinstance(layer, base_layer.Layer):
             raise TypeError(
                 'The added layer must be '
@@ -20,6 +22,7 @@ class Sequential(keras_Sequential):
             )
         self.built = False
 
+        # source code L172
         if self.outputs:
             # If the model is being built continuously on top of an input layer:
             # refresh its output.
