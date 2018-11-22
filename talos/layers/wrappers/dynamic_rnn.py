7c59e84c29d15f4cd327b202fdb1332478b04e5c
noobOriented <jsaon92@gmail.com>
2018-11-23 01:10:50 +0800

    let dynamic_rnn can return sequences.

--- talos/layers/wrappers/dynamic_rnn.py
+++ talos/layers/wrappers/dynamic_rnn.py
@@ -15,9 +15,10 @@ def _valid_rnn_cell(cell):
 
 class DynamicRecurrent(tf.keras.layers.Wrapper):
 
-    def __init__(self, cell):
+    def __init__(self, cell, return_sequences=False):
         _valid_rnn_cell(cell)
         super().__init__(layer=cell)
+        self.return_sequences = return_sequences
 
     def call(self, inputs, seqlen=None, initial_state=None):
         outputs, state = tf.nn.dynamic_rnn(
@@ -27,6 +28,8 @@ class DynamicRecurrent(tf.keras.layers.Wrapper):
             initial_state=initial_state,
             dtype=inputs.dtype,
         )  # shape (N, T, D_out), [(N, D_state), ...]
+        if self.return_sequences:
+            return outputs
         if hasattr(state, 'h'):
             return state.h
         if hasattr(state, '__len__'):
@@ -35,4 +38,6 @@ class DynamicRecurrent(tf.keras.layers.Wrapper):
 
     def compute_output_shape(self, input_shape):
         input_shape = input_shape.as_list()
+        if self.return_sequences:
+            return tf.TensorShape([input_shape[0], input_shape[1], self.layer.units])
         return tf.TensorShape([input_shape[0], self.layer.units])
