19600319e7eea91271502a16b5584d8c67291d87
noobOriented <jsaon92@gmail.com>
2019-01-24 18:51:01 +0800

    reorganize pooling tests.

--- talos/layers/tests/test_pooling.py
+++ talos/layers/tests/test_pooling.py
@@ -3,113 +3,115 @@ import pytest
 import numpy as np
 import tensorflow as tf
 
-from talos.module import Sequential
 from ..pooling import GlobalAttentionPooling1D
 from ..pooling import GlobalAveragePooling1D
 
 
-def test_global_attention_pooling_1d():
-    width, channel = 10, 4
-    units, heads = 3, 5
-    att_pool = GlobalAttentionPooling1D(units=units, heads=heads)
-    inputs = tf.placeholder(dtype=tf.float32, shape=[None, width, channel])
-    outputs = att_pool(inputs)
+class TestGlobalPooling1D:
 
-    assert outputs.shape.as_list() == [None, heads, channel]
-    assert att_pool.compute_output_shape(inputs.shape).as_list() == [None, heads, channel]
-    assert len(att_pool.losses) == 0
+    @pytest.fixture(scope='class')
+    def inputs(self):
+        return tf.placeholder(tf.float32, [None, 5, 1])
 
+    @pytest.fixture(scope='class')
+    def seqlen(self):
+        return tf.placeholder(tf.int32, [None])
 
-def test_global_attention_pooling_1d_reuse():
-    channel, units, heads = 3, 4, 5
-    att_pool = GlobalAttentionPooling1D(units=units, heads=heads, reg_coeff=1.0)
-    many_inputs = [
-        tf.zeros([batch_size, width, channel])
-        for batch_size, width in zip([1, 2, 3], [4, 5, 6])
-    ]
-    # try: can call on any rank 3 tensor with same last dim.
-    [att_pool(inputs) for inputs in many_inputs]
+    @pytest.fixture(scope='class')
+    def layer(self):
+        return GlobalAveragePooling1D()
 
-    losses = att_pool.losses
-    assert len(losses) == len(many_inputs)  # any input has its reg loss
-    assert all(loss.shape.as_list() == [] for loss in losses)
+    def test_mask_value(self, sess, inputs, seqlen, layer):
+        outputs = layer(inputs, seqlen=seqlen)
 
+        inputs_val = np.array([
+            [0., 1., 2., 3., 4.],
+            [2., 3., 4., 5., 6.],
+        ]).reshape(-1, 5, 1)
 
-@pytest.mark.parametrize('invalid_inputs', [
-    tf.zeros(shape=[2, 3]),
-    tf.zeros(shape=[2, 3, 1, 1]),
-])
-def test_global_attention_pooling_1d_invalid_input_rank(invalid_inputs):
-    att_pool = GlobalAttentionPooling1D(units=3, heads=4)
-    with pytest.raises(ValueError):
-        att_pool(invalid_inputs)
+        sess.run(tf.variables_initializer(var_list=layer.variables))
+        outputs_val = sess.run(
+            outputs,
+            feed_dict={
+                inputs: inputs_val,
+                seqlen: [2, 4],
+            },
+        )
 
+        expected_outputs_val = np.array([(0. + 1.) / 2., (2. + 3. + 4. + 5.) / 4.]).reshape(-1, 1)
+        np.testing.assert_array_almost_equal(outputs_val, expected_outputs_val)
 
-def test_global_attention_pooling_1d_value(sess):
-    batch_size, maxlen = 3, 10
-    att_pool = GlobalAttentionPooling1D(units=3, heads=4)
-    inputs = tf.random_normal(shape=[batch_size, maxlen, 10])
-    seqlen = tf.random_uniform(
-        minval=3,
-        maxval=maxlen,
-        shape=[batch_size],
-        dtype=tf.int32,
-    )
-    attended_vec = att_pool(inputs, seqlen=seqlen)
-
-    sess.run(tf.variables_initializer(var_list=att_pool.variables))
-    inputs_val, seqlen_val, attended_vec_val = sess.run(
-        [inputs, seqlen, attended_vec])
-
-    # since attend_vec is a weighted average of inputs
-    for row, length, vec in zip(inputs_val, seqlen_val, attended_vec_val):
-        min_val = np.min(row[:length], axis=0, keepdims=True)
-        max_val = np.max(row[:length], axis=0, keepdims=True)
-        assert np.logical_and(min_val < vec, vec < max_val).all()
+    def test_support_masked_inputs(self, inputs, layer):
+        masked_inputs = tf.keras.layers.Masking()(inputs)
+        layer(masked_inputs)
 
 
-def test_sequential_attention():
-    att_pool = GlobalAttentionPooling1D(units=3, heads=4)
-    seq = Sequential([att_pool])
-    inputs = tf.random_normal(shape=[1, 3, 10])
-    seqlen = tf.random_uniform(
-        minval=1,
-        maxval=3,
-        shape=[1],
-        dtype=tf.int32,
-    )
-    seq(inputs, seqlen=seqlen)
+class TestGlobalAttentionPooling1D:
 
+    @pytest.fixture(scope='class')
+    def inputs(self):
+        return tf.placeholder(dtype=tf.float32, shape=[None, 4, 3])
 
-def test_average_pooling_mask_value(sess):
-    pool = GlobalAveragePooling1D()
-    inputs = tf.placeholder(tf.float32, [None, 5, 1])
-    seqlen = tf.placeholder(tf.int32, [None])
-    outputs = pool(inputs, seqlen=seqlen)
+    @pytest.fixture(scope='class')
+    def seqlen(self):
+        return tf.placeholder(dtype=tf.int32, shape=[None])
 
-    inputs_val = np.array([
-        [[0], [1], [2], [3], [4]],
-        [[2], [3], [4], [5], [6]],
-    ], dtype=np.float32)
+    @pytest.fixture(scope='class')
+    def layer(self):
+        return GlobalAttentionPooling1D(units=3, heads=5)
 
-    sess.run(tf.variables_initializer(var_list=pool.variables))
-    outputs_val = sess.run(
-        outputs,
-        feed_dict={
-            inputs: inputs_val,
-            seqlen: [2, 4],
-        },
-    )
+    def test_output_shape(self, inputs, layer):
+        width, channel = inputs.shape.as_list()[1:]
+        heads = layer.heads
+        outputs = layer(inputs)
 
-    expected_outputs_val = np.array([[0.5], [3.5]], dtype=np.float32)
-    np.testing.assert_array_almost_equal(outputs_val, expected_outputs_val)
+        assert outputs.shape.as_list() == [None, heads, channel]
+        assert layer.compute_output_shape(inputs.shape).as_list() == [None, heads, channel]
+        assert len(layer.losses) == 0
 
+    def test_regularization_losses(self):
+        channel, units, heads = 3, 4, 5
+        att_pool = GlobalAttentionPooling1D(units=units, heads=heads, reg_coeff=1.0)
+        many_inputs = [
+            tf.zeros([batch_size, width, channel])
+            for batch_size, width in zip([1, 2, 3], [4, 5, 6])
+        ]
+        # try: can call on any rank 3 tensor with same last dim.
+        [att_pool(inputs) for inputs in many_inputs]
 
-def test_given_mask():
-    att_pool = GlobalAttentionPooling1D(units=3, heads=4)
-    pool = GlobalAveragePooling1D()
-    inputs = tf.placeholder(tf.float32, [None, 5, 1])
-    masked_inputs = tf.keras.layers.Masking()(inputs)
+        losses = att_pool.losses
+        assert len(losses) == len(many_inputs)  # any input has its reg loss
+        assert all(loss.shape.as_list() == [] for loss in losses)
 
-    att_pool(masked_inputs)
-    pool(masked_inputs)
+    @pytest.mark.parametrize('invalid_inputs', [
+        tf.zeros(shape=[2, 3]),
+        tf.zeros(shape=[2, 3, 1, 1]),
+    ])
+    def test_raise_invalid_input_rank(self, invalid_inputs, layer):
+        with pytest.raises(ValueError):
+            layer(invalid_inputs)
+
+    def test_output_value(self, sess, inputs, seqlen):
+        maxlen, channel = inputs.shape.as_list()[1:]
+        # zero init to isolate the grad through weights calculation.
+        att_pool = GlobalAttentionPooling1D(units=3, heads=4, kernel_initializer='zeros')
+        attended_vec = att_pool(inputs, seqlen=seqlen)
+        grads = tf.gradients(attended_vec, inputs)[0]  # same shape as inputs
+
+        seqlen_batch = np.random.randint(2, maxlen + 1, size=[5])
+        sess.run(tf.variables_initializer(var_list=att_pool.variables))
+        grad_batch = sess.run(
+            grads,
+            feed_dict={
+                inputs: [np.random.rand(maxlen, channel) for _ in seqlen_batch],
+                seqlen: seqlen_batch,
+            },
+        )
+        for seqlen_sample, grad_sample in zip(seqlen_batch, grad_batch):
+            attended_section = grad_sample[:seqlen_sample]
+            assert np.logical_and(0. < attended_section, attended_section < att_pool.heads).all()
+            assert (grad_sample[seqlen_sample:] == 0.).all()
+
+    def test_support_masked_inputs(self, inputs, layer):
+        masked_inputs = tf.keras.layers.Masking()(inputs)
+        layer(masked_inputs)
