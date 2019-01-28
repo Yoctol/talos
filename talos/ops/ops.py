68c016cda406f0eb33b30b71da1c65a3c5d711d7
noobOriented <jsaon92@gmail.com>
2019-01-28 15:53:05 +0800

    impl/test masked_reduce_mean.

--- talos/ops/ops.py
+++ talos/ops/ops.py
@@ -1,20 +1,35 @@
 import tensorflow as tf
 
 
-def sequence_reduce_mean(
-        input_tensor: tf.Tensor,
-        lengths: tf.Tensor,
+def masked_reduce_mean(
+        inputs: tf.Tensor,  # shape (N, T)
+        mask: tf.Tensor,  # shape (N, T)
+        true_count: tf.Tensor = None,
     ):
-    dtype = input_tensor.dtype
-    maxlen = input_tensor.shape[1].value
-    if maxlen is None:
-        raise ValueError("'maxlen' should be known!")
-    seq_mask = tf.sequence_mask(lengths, maxlen=maxlen, dtype=dtype)
+    mask = tf.cast(mask, inputs.dtype)
 
-    masked_input = input_tensor * seq_mask
-    masked_input_per_seq = tf.reduce_sum(masked_input, axis=1)
+    masked_input = mask * inputs  # shape (N, T)
+    sum_per_seq = tf.reduce_sum(masked_input, axis=1)  # shape (N, )
 
-    eps = tf.keras.backend.epsilon()
-    mean_per_seq = masked_input_per_seq / (tf.cast(lengths, dtype) + eps)
+    if true_count is None:  # use pre computed true_count
+        true_count = tf.reduce_sum(mask, axis=1)  # shape (N, )
+    else:
+        true_count = tf.cast(true_count, inputs.dtype)
+
+    mean_per_seq = sum_per_seq / (true_count + tf.keras.backend.epsilon())
     mean_per_batch = tf.reduce_mean(mean_per_seq)
     return mean_per_batch
+
+
+def sequence_reduce_mean(
+        inputs: tf.Tensor,
+        lengths: tf.Tensor,
+    ):
+    maxlen = inputs.shape[1].value
+    seq_mask = tf.sequence_mask(lengths, maxlen=maxlen, dtype=inputs.dtype)
+
+    return masked_reduce_mean(
+        inputs,
+        mask=seq_mask,
+        true_count=lengths,
+    )
--- talos/ops/tests/test_ops.py
+++ talos/ops/tests/test_ops.py
@@ -1,6 +1,24 @@
 import tensorflow as tf
 
-from ..ops import sequence_reduce_mean
+from ..ops import masked_reduce_mean, sequence_reduce_mean
+
+
+def test_masked_reduce_mean(sess):
+    x = tf.constant([
+        [2, 3, 4, 5],
+        [0, 2, 4, 6],
+    ], dtype=tf.float32)
+    mask = tf.constant([
+        [True, False, True, False],
+        [False, True, True, True],
+    ], dtype=tf.bool)
+    mean_x = masked_reduce_mean(x, mask)
+
+    expected_mean_x = (
+        (2 + 4) / 2 + (2 + 4 + 6) / 3
+    ) / 2
+
+    assert sess.run(mean_x) == expected_mean_x
 
 
 def test_sequence_reduce_mean(sess):
