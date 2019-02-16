04dc83c75f190e0147df9f210b5716c02c861489
noobOriented <jsaon92@gmail.com>
2019-02-16 18:48:30 +0800

    modify several syntax (early return).

--- talos/decorators/spectral_norm.py
+++ talos/decorators/spectral_norm.py
@@ -9,10 +9,9 @@ _WEIGHTS_VARIABLE_NAME = "kernel"
 def add_spectral_norm(
         layer: tf.layers.Layer,
         kernel_name: Set[str] = None,
-        eps: float = 1e-7,
     ):
-    # Very Very Evil HACK
-    assert not layer.built, "Can't add spectral norm on built layer!"
+    if layer.built:
+        raise ValueError("Can't add spectral norm on built layer!")
 
     def is_kernel_var(name):
         if kernel_name is not None:
@@ -22,32 +21,42 @@ def add_spectral_norm(
 
     original_add_weight = layer.add_weight
 
-    def new_add_weight(self, name, shape, **kwargs):
+    # Very Very Evil HACK
+    def new_add_weight(self, name=None, shape=None, **kwargs):
         kernel = original_add_weight(name, shape, **kwargs)
-        if is_kernel_var(name):
-            assert len(shape) >= 2, "Can't apply spectral norm on variable rank < 2!"
-            if len(shape) > 2:
-                kernel_matrix = tf.reshape(kernel, [-1, kernel.shape[-1]])
-            else:
-                kernel_matrix = kernel
+        if not is_kernel_var(name):
+            return kernel
 
-            u_vector = original_add_weight(
-                name=f'{name}/singular_vector',
-                shape=(kernel_matrix.shape[0].value, 1),
-                initializer=tf.truncated_normal_initializer(),
-                trainable=False,
-                dtype=kernel.dtype,
-            )
-            v_vector = tf.matmul(kernel_matrix, u_vector, transpose_a=True)
-            spectral_norm = tf.norm(v_vector, axis=0)
-            new_u = tf.nn.l2_normalize(kernel_matrix @ v_vector, axis=0)
-            update_u = tf.assign(
-                u_vector, new_u, name=f'{name}/power_iter',
-            )
-            self.add_update(update_u)
+        if len(shape) < 2:
+            raise ValueError("Can't apply spectral norm on variable rank < 2!")
 
-            kernel = tf.truediv(kernel, spectral_norm + eps, name=f'{name}_sn')
+        kernel_matrix = to_rank2(kernel)  # shape (U, V)
 
-        return kernel
+        u_vector = original_add_weight(
+            name=f'{name}/singular_vector',
+            shape=(kernel_matrix.shape[0].value, 1),
+            initializer=tf.keras.initializer.lecun_normal(),  # unit vector
+            trainable=False,
+            dtype=kernel.dtype,
+        )  # shape (U, 1)
+        v_vector = tf.matmul(kernel_matrix, u_vector, transpose_a=True)  # shape (V, 1)
+        spectral_norm = tf.norm(v_vector)  # shape (1)
+        normed_kernel = tf.truediv(
+            kernel,
+            spectral_norm + tf.keras.backend.epsilon(),
+            name=f'{name}_sn',
+        )
+
+        new_u = tf.nn.l2_normalize(kernel_matrix @ v_vector, axis=0)  # shape (U, 1)
+        update_u = tf.assign(u_vector, new_u, name=f'{name}/power_iter')
+        self.add_update(update_u)
+
+        return normed_kernel
 
     layer.add_weight = types.MethodType(new_add_weight, layer)
+
+
+def to_rank2(tensor: tf.Tensor):
+    if tensor.shape.ndims > 2:
+        return tf.reshape(tensor, [-1, tensor.shape[-1].value])
+    return tensor
