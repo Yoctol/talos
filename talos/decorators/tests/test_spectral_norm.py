b7c27e6b811eb94fdd4c199c0bf62c60995cc01e
noobOriented <jsaon92@gmail.com>
2019-02-16 20:23:57 +0800

    add more test case.

--- talos/decorators/tests/test_spectral_norm.py
+++ talos/decorators/tests/test_spectral_norm.py
@@ -1,5 +1,6 @@
 import pytest
 
+import numpy as np
 import tensorflow as tf
 
 from ..spectral_norm import add_spectral_norm
@@ -9,32 +10,38 @@ from ..spectral_norm import add_spectral_norm
     (tf.keras.layers.Dense(10), tf.zeros([3, 4])),
     (tf.keras.layers.Conv1D(filters=10, kernel_size=3), tf.zeros([3, 4, 5])),
     (tf.keras.layers.Conv2D(filters=10, kernel_size=3), tf.zeros([3, 4, 5, 5])),
+    (tf.keras.layers.GRUCell(10), tf.zeros([3, 4])),
+    (tf.keras.layers.LSTMCell(10), tf.zeros([3, 4])),
 ])
-def test_spectral_norm_single_kernel(layer, inputs):
+def test_spectral_norm_single_kernel(layer, inputs, sess):
     add_spectral_norm(layer)
-    layer(inputs)
-
-    assert len(layer.updates) == 1
-    assert layer.kernel.name.endswith('_sn:0')
-
-
-@pytest.mark.parametrize('layer', [
-    tf.keras.layers.GRUCell(10),
-    tf.keras.layers.LSTMCell(10),
-])
-def test_spectral_norm_rnn(layer):
-    inputs = tf.zeros([3, 4])
-    state = layer.get_initial_state(inputs)
-    if not isinstance(state, list):
-        state = [state]
-    add_spectral_norm(layer)
-    layer(inputs, state)
+    if hasattr(layer, 'state_size'):
+        state = layer.get_initial_state(inputs)
+        if not isinstance(state, list):
+            state = [state]
+        layer(inputs, state)
+    else:
+        layer(inputs)
 
     kernel_list = [
         attr
         for attr_name, attr in layer.__dict__.items()
         if attr_name.endswith('kernel')
     ]
+    u_vector_list = layer.non_trainable_variables
 
-    assert len(layer.updates) == len(kernel_list)
-    assert [kernel.name.endswith('_sn:0') for kernel in kernel_list]
+    assert len(layer.updates) == len(u_vector_list) == len(kernel_list)
+    assert all([kernel.name.endswith('_sn:0') for kernel in kernel_list])
+
+    sess.run(tf.variables_initializer(layer.variables))
+
+    u_vector_val_list = sess.run(u_vector_list)
+    sess.run(layer.updates)
+    updated_u_vector_val_list = sess.run(u_vector_list)
+
+    for u_vector_val, updated_u_vector_val in zip(u_vector_val_list, updated_u_vector_val_list):
+        with pytest.raises(AssertionError):
+            np.testing.assert_array_almost_equal(
+                u_vector_val,
+                updated_u_vector_val,
+            )
