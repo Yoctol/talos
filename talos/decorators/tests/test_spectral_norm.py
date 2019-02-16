2317f53e227a8d2b8e9970bff187940f06d85b57
noobOriented <jsaon92@gmail.com>
2019-02-16 20:44:55 +0800

    add Sequential test case.

--- talos/decorators/tests/test_spectral_norm.py
+++ talos/decorators/tests/test_spectral_norm.py
@@ -1,19 +1,30 @@
+from itertools import chain
 import pytest
 
 import numpy as np
 import tensorflow as tf
 
+from talos.layers import (
+    Conv1D,
+    Conv2D,
+    Dense,
+    GRU,
+    GRUCell,
+    LSTM,
+    LSTMCell,
+)
 from ..spectral_norm import add_spectral_norm
 
 
 @pytest.mark.parametrize('layer,inputs', [
-    (tf.keras.layers.Dense(10), tf.zeros([3, 4])),
-    (tf.keras.layers.Conv1D(filters=10, kernel_size=3), tf.zeros([3, 4, 5])),
-    (tf.keras.layers.Conv2D(filters=10, kernel_size=3), tf.zeros([3, 4, 5, 5])),
-    (tf.keras.layers.GRUCell(10), tf.zeros([3, 4])),
-    (tf.keras.layers.LSTMCell(10), tf.zeros([3, 4])),
-    (tf.keras.layers.GRU(10), tf.zeros([3, 4, 5])),
-    (tf.keras.layers.LSTM(10), tf.zeros([3, 4, 5])),
+    (Dense(10), tf.zeros([3, 4])),
+    (Conv1D(filters=10, kernel_size=3), tf.zeros([3, 4, 5])),
+    (Conv2D(filters=10, kernel_size=3), tf.zeros([3, 4, 5, 5])),
+    (GRUCell(10), tf.zeros([3, 4])),
+    (LSTMCell(10), tf.zeros([3, 4])),
+    (GRU(10), tf.zeros([3, 4, 5])),
+    (LSTM(10), tf.zeros([3, 4, 5])),
+    (tf.keras.Sequential([Dense(10), Dense(10), Dense(10)]), tf.zeros([3, 4])),
 ])
 def test_spectral_norm_for_layer(layer, inputs, sess):
     add_spectral_norm(layer)
@@ -25,7 +36,7 @@ def test_spectral_norm_for_layer(layer, inputs, sess):
     else:
         layer(inputs)
 
-    kernel_list = recursive_get_kernels(layer)
+    kernel_list = recursive_get_kernel_attributes(layer)
     u_vector_list = layer.non_trainable_variables
 
     assert len(layer.updates) == len(u_vector_list) == len(kernel_list)
@@ -45,9 +56,15 @@ def test_spectral_norm_for_layer(layer, inputs, sess):
             )
 
 
-def recursive_get_kernels(layer):
-    if hasattr(layer, 'cell'):
-        layer = layer.cell
+def recursive_get_kernel_attributes(layer):
+    if isinstance(layer, tf.keras.Sequential):
+        return list(chain.from_iterable([
+            recursive_get_kernel_attributes(layer)
+            for layer in layer.layers
+        ]))
+
+    if isinstance(layer, tf.keras.layers.RNN):
+        return recursive_get_kernel_attributes(layer.cell)
 
     return [
         attr
