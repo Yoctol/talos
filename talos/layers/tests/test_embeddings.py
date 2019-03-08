640bf86745cb188368927adb2edd66eaffa80ea5
Solumilken <s916526000@gmail.com>
2019-03-08 12:22:16 +0800

    constant embedding layer can be frozen

--- talos/layers/tests/test_embeddings.py
+++ talos/layers/tests/test_embeddings.py
@@ -2,6 +2,7 @@ import pytest
 
 import numpy as np
 import tensorflow as tf
+from tensorflow.python.framework import graph_util
 
 from ..embeddings import Embedding
 from talos.networks import Sequential
@@ -83,3 +84,60 @@ def test_construct_from_weights(inputs, sess, constant):
 def test_construct_from_invalid_weights_raise(invalid_weights):
     with pytest.raises(ValueError):
         Embedding.from_weights(invalid_weights)
+
+
+def test_freeze(inputs, sess):
+    # build graph with constant embedding layer
+    embed_layer = Embedding.from_weights(np.random.rand(5, 10), constant=True)
+    outputs = embed_layer(inputs)
+
+    # freeze graph
+    frozen_graph_def = graph_util.convert_variables_to_constants(
+        sess=sess,
+        input_graph_def=sess.graph_def,
+        output_node_names=[outputs.op.name],  # node name == op name
+    )
+
+    # check frozen graph have the same embedding as before
+    new_sess = create_session_from_graphdef(frozen_graph_def)
+    new_inputs = new_sess.graph.get_tensor_by_name(inputs.name)
+    new_outputs = new_sess.graph.get_tensor_by_name(outputs.name)
+
+    maxlen = inputs.shape[1].value
+    inputs_val = np.random.choice(embed_layer.vocab_size, size=[5, maxlen])
+    outputs_val = sess.run(outputs, feed_dict={inputs: inputs_val})
+    new_outputs_val = new_sess.run(new_outputs, feed_dict={new_inputs: inputs_val})
+
+    np.testing.assert_array_almost_equal(outputs_val, new_outputs_val)
+
+
+def test_freeze_fail(inputs, sess):
+    # build graph with variable embedding layer
+    weights = np.array([[0, 1], [2, 3], [4, 5]], dtype=np.float32)
+    embed_layer = Embedding.from_weights(weights, constant=False)
+    outputs = embed_layer(inputs)
+
+    sess.run(tf.variables_initializer(var_list=embed_layer.variables))
+
+    frozen_graph_def = graph_util.convert_variables_to_constants(
+        sess=sess,
+        input_graph_def=sess.graph_def,
+        output_node_names=[outputs.op.name],  # node name == op name
+    )
+
+    with pytest.raises(ValueError):
+        create_session_from_graphdef(frozen_graph_def)
+
+
+def create_session_from_graphdef(graph_def):
+    """
+    Create new session from given tf.GraphDef object
+    Arg:
+       graph_def (tf.GraphDef): a tf.GraphDef object
+    Return:
+       session (tf.Session): a new session with given graph_def
+    """
+    new_sess = tf.Session(graph=tf.Graph())
+    with new_sess.graph.as_default():
+        tf.import_graph_def(graph_def, name="")
+    return new_sess
