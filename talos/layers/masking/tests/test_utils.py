894abefd306a6be3d27a866c0ef41e236668c80c
noobOriented <jsaon92@gmail.com>
2019-05-18 23:37:55 +0800

    extract apply_mask to masking/utils/

---
+++ talos/layers/masking/tests/test_utils.py
@@ -0,0 +1,18 @@
+import numpy as np
+import tensorflow as tf
+
+from ..utils import apply_mask
+
+
+def test_apply_mask(sess):
+    x = tf.constant([[[0, 1], [2, 3], [4, 5], [6, 7], [8, 9]]], dtype=tf.float32)
+    assert apply_mask(x) is x
+
+    mask = tf.constant([[True, False, True, True, False]])
+    out = apply_mask(x, mask)
+
+    assert out._keras_mask is mask
+    np.testing.assert_array_equal(
+        sess.run(out),
+        [[[0., 1.], [0., 0.], [4., 5.], [6., 7.], [0., 0.]]],
+    )
---
+++ talos/layers/masking/utils.py
@@ -0,0 +1,21 @@
+import tensorflow as tf
+
+
+def apply_mask(inputs: tf.Tensor, mask: tf.Tensor = None) -> tf.Tensor:
+    if mask is not None:
+        i_rank = inputs.shape.ndims
+        m_rank = mask.shape.ndims
+        casted_mask = tf.cast(mask, inputs.dtype)
+        if i_rank > m_rank:
+            indices = tuple(
+                slice(None) if d < m_rank else tf.newaxis
+                for d in range(i_rank)
+            )  # expand multiple dims
+            inputs *= casted_mask[indices]
+        elif i_rank == m_rank:
+            inputs *= casted_mask
+        else:
+            raise ValueError(f"Invalid mask rank > inputs rank! {m_rank} > {i_rank}")
+
+    inputs._keras_mask = mask
+    return inputs
