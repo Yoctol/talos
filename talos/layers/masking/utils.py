110ece0ccbf28482b02718966b5d5a04f44cebd9
noobOriented <jsaon92@gmail.com>
2019-05-19 01:44:19 +0800

    check inputs.op.name to avoid redundant multiply

--- talos/layers/masking/utils.py
+++ talos/layers/masking/utils.py
@@ -1,20 +1,27 @@
+import re
+
 import tensorflow as tf
 
 
-def apply_mask(inputs: tf.Tensor, mask: tf.Tensor = None) -> tf.Tensor:
-    if mask is not None:
-        i_rank = inputs.shape.ndims
-        m_rank = mask.shape.ndims
-        casted_mask = tf.cast(mask, inputs.dtype)
-        if i_rank > m_rank:
-            indices = tuple(
-                slice(None) if d < m_rank else tf.newaxis
-                for d in range(i_rank)
-            )  # expand multiple dims
-            inputs *= casted_mask[indices]
-        elif i_rank == m_rank:
-            inputs *= casted_mask
-        else:
-            raise ValueError(f"Invalid mask rank > inputs rank! {m_rank} > {i_rank}")
+_APPLY_MASK_MUL = 'apply_mask_mul'
 
-    return inputs
+
+def apply_mask(inputs: tf.Tensor, mask: tf.Tensor) -> tf.Tensor:
+    if mask is None:
+        return inputs
+    if re.match(f".*{_APPLY_MASK_MUL}(_\d+)?", inputs.op.name):  # e.g. .../apply_mask_mul_1
+        return inputs  # mask has already been applied
+
+    i_rank = inputs.shape.ndims
+    m_rank = mask.shape.ndims
+    mask = tf.cast(mask, inputs.dtype)
+    if i_rank > m_rank:
+        indices = tuple(
+            slice(None) if d < m_rank else tf.newaxis
+            for d in range(i_rank)
+        )  # expand multiple dims
+        mask = mask[indices]
+    elif i_rank < m_rank:
+        raise ValueError(f"Invalid mask rank > inputs rank! {m_rank} > {i_rank}")
+
+    return tf.multiply(inputs, mask, name=_APPLY_MASK_MUL)
